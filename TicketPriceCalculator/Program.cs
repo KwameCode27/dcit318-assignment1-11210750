7391a579e26fc27f5b92f44f44af65e45577ef6e
fente <mickeyfent3@gmail.com>
2025-08-04 17:28:14 +0000

    Ticket Price Calculator  Project uploaded

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Movie Ticket Price Calculator!");
+
+            // Get user input
+            Console.Write("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // Determine ticket price
+            int ticketPrice;
+            if (age <= 12 || age >= 65)
+                ticketPrice = 7;
+            else
+                ticketPrice = 10;
+
+            // Display result
+            Console.WriteLine($"Your ticket price is: GHC{ticketPrice}");
+        }
+    }
+}
+
